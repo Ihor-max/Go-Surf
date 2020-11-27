37d95d00fde281b5b48bca5c97beeb7dd382d5bc
ihor-max <ihorek85@gmail.com>
2020-11-27 10:42:35 +0200

    Верстка блока Travel

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,9 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="/app/img/arrow-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="/app/img/arrow.svg" alt=""></img>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
@@ -16,7 +16,25 @@ $(function () {
     $('.surf-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-    })
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="/app/img/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="/app/img/arrow.svg" alt=""></img>',
+        asNavFor: '.slider-map',
+    });
+    $('.slider-map').slick({
+         slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+         focusOnSelect: true,
+
+    });
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="/app/img/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="/app/img/arrow.svg" alt=""></img>',
+
+    });
 
 
 });
