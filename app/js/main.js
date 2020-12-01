c0e18de0588f2ccf20c4e9d3706923b8faa36e50
ihor-max <ihorek85@gmail.com>
2020-12-01 17:45:23 +0200

    shop

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,54 @@ $(function () {
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="/app/img/arrow.svg" alt=""></img>',
 
     });
+        jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="./img/Plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="./img/Minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function() {
+      var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+    $('.quantity-button').on('click', function () {
+      
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    $('.summ').html('$'+summ);  
+    });
+
+
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+  $('.summ').html('$' + summ);
+  
+
+  $('.surfboard-box__circle').on('click', function () {
+    $(this).toggleClass('active')
+
+  });
+    
 
 
 });
